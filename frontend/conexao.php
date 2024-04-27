429dc7e6e73c4ea3aaa0e9746b5984c8e6accd2e
Geni <geni@grupositi.com>
2024-04-27 19:21:20 -0300

    página para lincar ao banco de dados

---
+++ frontend/conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servidor = "localhost";
+$usuario ="jrfei570_membros";
+$senha = "tj109652";
+$dbname = "jrfei570_ti";
+
+$conexao = mysqli_connect($servidor,$usuario, $senha, $dbname) or die("erro");
+
+?>
