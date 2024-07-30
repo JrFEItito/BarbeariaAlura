377d55e27ad031bb4b1609d9e144aa9e4dae2022
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2024-07-30 17:39:41 -0300

    Update logica.php

--- frontend/logica.php
+++ frontend/logica.php
@@ -1,17 +1,21 @@
 <?php
+
 session_start();
 include_once("conexao.php");
 
-if(isset($_POST['Enviar'])){
-    $mensagem = $_POST['Mensagem']; // Corrigido o nome do campo
-    $inserir = "INSERT INTO PDI_GitHub_Barbearia(ID, MENSAGEM) VALUES ('','$mensagem')"; // Corrigido a concatenação da variável $mensagem
+if (isset($_POST['Enviar'])) {
+    $mensagem = mysqli_real_escape_string($conexao, $_POST['Mensagem']); // Escapar a mensagem para evitar SQL Injection
+    $inserir = "INSERT INTO PDI_GitHub_Barbearia (MENSAGEM) VALUES ('$mensagem')"; // Removido valor vazio do ID
     $sql = mysqli_query($conexao, $inserir);
-    if($sql) {
+    
+    if ($sql) {
         $_SESSION['Mensagem'] = "Mensagem enviada com sucesso!";
         header("Location: contato.html");
     } else {
         $_SESSION['Mensagem'] = "Erro ao enviar a mensagem. Por favor, tente novamente mais tarde.";
         header("Location: contato.html");
     }
+    exit(); 
 }
-    
+?>
+
