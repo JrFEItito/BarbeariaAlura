dd9f0ee3358aa2144e6cc36002849cacaa3bc95b
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2024-07-30 17:32:56 -0300

    Update conexao.php

--- frontend/conexao.php
+++ frontend/conexao.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
 $usuario ="jrfei570_membros";
 $senha = "tj109652";
-$dbname = "jrfei570_ti";
+$dbname = "jrfei570_CursoPHP";
 
 $conexao = mysqli_connect($servidor,$usuario, $senha, $dbname) or die("erro");
 
